b178210dd0aa8e16d85a9cf2e6ebc79ffa39ae94
Stijn Hooft <stijnhooft@hotmail.com>
2012-03-21 21:23:19 +0100

    git test

--- sprint3/src/views/Start.java
+++ sprint3/src/views/Start.java
@@ -34,7 +34,7 @@ import controllers.Databank;
 import controllers.Login;
 
 import model.Model;
-/**GIT TEST**/
+/**GIT TEST WERKT BIJ STIJN**/
 @SuppressWarnings("serial")
 public class Start extends JPanel implements ActionListener
 {	
